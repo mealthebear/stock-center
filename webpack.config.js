5732fff34b12981feb384b701f00f32e1d74f983
mealthebear <mealthebear@gmail.com>
2021-08-29 13:31:48 -0700

    Created successful webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
+  devtool: 'source-map',
   entry: `${SRC_DIR}/index.js`,
   output: {
     path: DIST_DIR,
     filename: 'bundle.js',
   },
+  mode: 'development',
   module: {
     rules: [
       {
