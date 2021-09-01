f96ed8b1c909aecce3c9374322dbe25c6ab9de00
mealthebear <mealthebear@gmail.com>
2021-08-31 22:43:02 -0700

    Applied flexbox styling to price banner

--- client/src/pages/Home/components/PriceBanner.js
+++ client/src/pages/Home/components/PriceBanner.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 
 const PriceBanner = () => {
+  // Add percent change later from data in API
   const mockStockData = [
     { stock: 'SPY', price: '450' },
     { stock: 'DJI', price: '35455' },
   ];
 
   return (
-    <div className="price-banner">
+    <div className='price-banner'>
       {mockStockData.map((stock, index) => {
-        return <div key={index}>{stock.stock} {stock.price}</div>
+        return <div className='stock-index' key={index}>
+          <span>{stock.stock}</span>
+          <span>{stock.price}</span>
+        </div>
       })}
     </div>
   )
