925cea4a978f339e4783d5e379323f3a996d53d7
mealthebear <mealthebear@gmail.com>
2022-01-26 14:27:34 -0800

    Created form component for searching stocks

---
+++ client/src/components/StockSearchForm.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+
+const StockSearchForm = () => {
+  return (
+    <form className='stock-search-form'>
+      <label className='form-header'>Which stocks strike your fancy today?</label>
+      <input className='form-input' placeholder={`Search - Ex. 'AAPL'`}></input>
+    </form>
+  )
+}
+
+export default StockSearchForm;
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import PriceBanner from '../../components/PriceBanner.js';
 import CoverPhoto from './CoverPhoto.js';
+import StockSearchForm from '../../components/StockSearchForm.js';
 
 const Home = () => {
   return (
     <>
       <PriceBanner />
       <CoverPhoto />
+      <StockSearchForm />
     </>
   )
 }
