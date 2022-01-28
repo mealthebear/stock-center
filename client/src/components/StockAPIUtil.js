54c99082e0c2fd4699a6bfa2d23fa775b2c514d1
mealthebear <mealthebear@gmail.com>
2022-01-27 16:20:20 -0800

    Created functions for different API endpoints

---
+++ client/src/components/StockAPIUtil.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const getCurrentDayClose = async (ticker, date) => {
+  const url = `https://api.polygon.io/v1/open-close/${ticker}/2020-10-14`;
+  const stockData = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${STOCKS_API_TOKEN}`,
+    },
+  });
+  return stockData;
+};
+
+const getPreviousClose = async (ticker) => {
+  const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/prev`;
+  const stockData = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${STOCKS_API_TOKEN}`,
+    },
+  });
+};
+
+const hasTradingClosed = () => {
+  /**
+   * hasTradingClosed uses 24 hour time to determine if
+   * trading has closed for the day. The reason for this is
+   * because hours and minutes in 24 hour time format
+   * concatenated together and then converted to an integer
+   * will always be greater than earlier times.
+   */
+  const today = new Date();
+  const endOfDayTrading = 1300;
+  let currentMinutes = today.getMinutes();
+  if (currentMinutes.toString().length === 1) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+  currentTime = Number(`${today.getHours()}${currentMinutes}`);
+  if (currentTime >= endOfDayTrading) {
+    return true;
+  } else {
+    return false;
+  };
+};
