4893426e55eef7e4b5fd03114e6a94cb306204d0
mealthebear <mealthebear@gmail.com>
2022-01-27 16:46:55 -0800

    Commented out unused functions

--- client/src/components/StockAPIUtil.js
+++ client/src/components/StockAPIUtil.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
-const getCurrentDayClose = async (ticker, date) => {
-  const url = `https://api.polygon.io/v1/open-close/${ticker}/2020-10-14`;
-  const stockData = await axios.get(url, {
-    headers: {
-      Authorization: `Bearer ${STOCKS_API_TOKEN}`,
-    },
-  });
-  return stockData;
-};
+/* Save function for later */
+// const getCurrentDayClose = async (ticker, date) => {
+//   const url = `https://api.polygon.io/v1/open-close/${ticker}/${date}`;
+//   const stockData = await axios.get(url, {
+//     headers: {
+//       Authorization: `Bearer ${STOCKS_API_TOKEN}`,
+//     },
+//   });
+//   return stockData;
+// };
 
 const getPreviousClose = async (ticker) => {
   const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/prev`;
@@ -17,26 +18,28 @@ const getPreviousClose = async (ticker) => {
       Authorization: `Bearer ${STOCKS_API_TOKEN}`,
     },
   });
+  return stockData;
 };
 
-const hasTradingClosed = () => {
-  /**
-   * hasTradingClosed uses 24 hour time to determine if
-   * trading has closed for the day. The reason for this is
-   * because hours and minutes in 24 hour time format
-   * concatenated together and then converted to an integer
-   * will always be greater than earlier times.
-   */
-  const today = new Date();
-  const endOfDayTrading = 1300;
-  let currentMinutes = today.getMinutes();
-  if (currentMinutes.toString().length === 1) {
-    currentMinutes = `0${currentMinutes}`;
-  }
-  currentTime = Number(`${today.getHours()}${currentMinutes}`);
-  if (currentTime >= endOfDayTrading) {
-    return true;
-  } else {
-    return false;
-  };
-};
+/* Save function for later */
+// const hasTradingClosed = () => {
+//   /**
+//    * hasTradingClosed uses 24 hour time to determine if
+//    * trading has closed for the day. The reason for this is
+//    * because hours and minutes in 24 hour time format
+//    * concatenated together and then converted to an integer
+//    * will always be greater than earlier times.
+//    */
+//   const today = new Date();
+//   const endOfDayTrading = 1300;
+//   let currentMinutes = today.getMinutes();
+//   if (currentMinutes.toString().length === 1) {
+//     currentMinutes = `0${currentMinutes}`;
+//   }
+//   currentTime = Number(`${today.getHours()}${currentMinutes}`);
+//   if (currentTime >= endOfDayTrading) {
+//     return true;
+//   } else {
+//     return false;
+//   };
+// };
